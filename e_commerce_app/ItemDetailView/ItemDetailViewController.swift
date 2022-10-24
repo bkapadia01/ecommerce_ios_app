ba47e8c4129d870366639bc87b299ef39504af79
Bhavin Kapadia <bkapadia@touchbistro.com>
2022-10-23 22:56:48 -0400

    Add item detail view

---
+++ e_commerce_app/ItemDetailView/ItemDetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ItemDetailViewController.swift
+//  e_commerce_app
+//
+//  Created by Bhavin Kapadia on 2022-10-16.
+//
+
+import Foundation
+import UIKit
+
+class ItemDetailViewController: UIViewController {
+    @IBOutlet weak var itemDetailViewImage: UIImageView!
+    @IBOutlet weak var itemDetailViewTitle: UILabel!
+    @IBOutlet weak var itemDetailViewDescription: UILabel!
+    
+   var selectedItem: WelcomeElement?
+//    {
+//        didSet {
+//            itemDetailViewTitle.text = selectedItem
+//            print(itemDetailViewTitle.text)
+//
+//        }
+//    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: nil, action: nil)
+        self.itemDetailViewTitle.text = selectedItem?.title ?? ""
+        self.itemDetailViewDescription.text = selectedItem?.welcomeDescription ?? ""
+        self.navigationController!.navigationBar.topItem?.backButtonTitle = "Return Home"
+
+        let productImageURL = selectedItem?.image
+        let url = URL(string: productImageURL!)
+        
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: url!)
+            DispatchQueue.main.async {
+                self.itemDetailViewImage.image = UIImage(data: data!)
+            }
+        }
+        
+    }
+}
