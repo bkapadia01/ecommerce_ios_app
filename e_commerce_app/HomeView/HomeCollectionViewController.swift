675b20e8652a165f68d81f098b50bf7c817f6337
Bhavin Kapadia <bkapadia@touchbistro.com>
2022-10-23 23:00:31 -0400

    segue to detail view identifier

--- e_commerce_app/HomeView/HomeCollectionViewController.swift
+++ e_commerce_app/HomeView/HomeCollectionViewController.swift
@@ -15,7 +15,8 @@ class HomeCollectionViewController: UICollectionViewController {
     private let homeViewModel: HomeViewModel
     private var productItems: [WelcomeElement] = []
     private let reuseIdentifier = "ProductCell"
-    
+    private let itemsPerRow: CGFloat = 2
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
     
     init?(homeViewModel: HomeViewModel, coder: NSCoder) {
         self.homeViewModel = homeViewModel
@@ -26,108 +27,99 @@ class HomeCollectionViewController: UICollectionViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        navigationItem.title = "Welcome " + homeViewModel.getLoggedInUsername()
+        navigationItem.title = "Welcome " + homeViewModel.getLoggedInUsername() + "!"
         
         homeViewModel.getAllProducts { result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let products):
-                    print(products)
                     self.productItems = products
-                  
+                    
                 case .failure(let error):
                     print(error)
                 }
-
+                
                 self.collectionView?.reloadData()
             }
         }
-        configureCollectionView()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+    }
 
-        // Register cell classes
-//        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedItem = productItems[indexPath.item]
+        self.performSegue(withIdentifier: "segueToItemDetailView", sender: selectedItem)
     }
     
-    func configureCollectionView() {
-        collectionView.backgroundColor = .lightGray
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
-
+    
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-//        let items = homeViewModel.products[section].count
-//        print(items.count)
         return productItems.count
     }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
-
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! HomeItemCollectionViewCell
     
-        // Configure the cell
-        print(productItems[indexPath.item].title)
-        cell.backgroundColor = .green
-        cell.itemLabel.text = productItems[indexPath.item].title
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! HomeItemCollectionViewCell
+        let productName = productItems[indexPath.item].title
+        let productImageURL = productItems[indexPath.item].image
+        let url = URL(string: productImageURL!)
+        
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: url!) // guard let display default broken image
+            DispatchQueue.main.async {
+                cell.itemImageView.image = UIImage(data: data!) // unwrap this
+                cell.layer.borderColor = UIColor.gray.cgColor
+                cell.layer.borderWidth = 1
+            }
+        }
+        // look into caching image using context/scratchpad
+        cell.itemLabel.text = productName
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let selectedItem = sender else {
+            return
+        }
+        
+        if segue.identifier == "segueToItemDetailView" {
+            guard let itemDetailVC = segue.destination as? ItemDetailViewController,
+                  let selectedIndexPath = self.collectionView.indexPathsForSelectedItems?.last?.row
+            else {
+                return
+            }
+            
+            itemDetailVC.selectedItem = productItems[selectedIndexPath]
+            
+            //            itemDetailVC.itemDetailViewTitle.text = productItems[selectedIndexPath].title
+            //            itemDetailVC.itemDetailViewTitle.text = productItems[selectedIndexPath].title
+            
+//            itemDetailVC.itemDetailViewTitle.text = productItems[selectedIndexPath].title ?? ""
+            print(productItems[selectedIndexPath].title ?? "")
+        }
     }
-    */
+}
 
+extension HomeCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath ) -> CGSize {
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView( _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int ) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView( _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int ) -> CGFloat {
+        return sectionInsets.left
+    }
 }
